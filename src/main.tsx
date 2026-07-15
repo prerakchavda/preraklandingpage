eebe3ad0b45dfcae08a4e3254a24876922963e15
Prerak Chavda <143364013+prerakchavda@users.noreply.github.com>
2026-07-15 18:13:31 -0500

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { PorscheDesignSystemProvider } from '@porsche-design-system/components-react';
 import App from './App';
 import './index.css';
 
